b9f301261cf445aa288cf70fb0a70304089614ef
Carlie Ostrom <rlietrom@gmail.com>
2017-07-21 12:10:11 -0700

    hi

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 var { User } = require('./models');
 var { Reminder } = require('./models');
+var rtm = require('./bot')
 
 mongoose.connection.on('connected', function() {
     console.log('yay, connected')
@@ -157,7 +158,7 @@ app.get('/connect/callback', function(req, res){
                     })
                     .then(function(mongoUser){
                         res.send('youre connected to google cal!');
-                        rtm.sendMessage('you are connected to google calandar');
+                        //rtm.sendMessage('you are connected to google calandar');
                     })
                 }
             })
